6481a217f08658b05ee0d8e777699219b1b5bb68
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-08 15:02:47 -0400

    Added 404 and error handling middleware

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
@@ -25,6 +32,16 @@ app.get("/employees/:id", (req, res) => {
   } else {
     res.status(404).send(`There is no employee with id ${id}.`);
   }
+});
+
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found."});
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong.");
 });
 
 app.listen(PORT, () => {
